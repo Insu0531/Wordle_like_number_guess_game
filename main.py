303f290980adba01b6b3264e877ff8b1d8f3ca97
insu0531 <kanginsu0531@gmail.com>
2025-11-24 21:48:11 +0900

    Fixed the length of the number to 5

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from logic import compare_num
 from logic import score
 
 print("*간단한 수 맞히기 게임*\n")
-n = int(input("몇 자리 수를 맞히시겠습니까?\n"))
-answer = number_generator(n)
+answer = number_generator(5)
 
 while True:
-    player_guess = guess(n)
+    player_guess = guess(5)
     print(score(player_guess, answer))
 
     if compare_num(player_guess, answer):
--- player.py
+++ player.py
@@ -1,10 +1,7 @@
-def guess(n): # n은 main에서 입력받은 정답의 자리수
+def guess(n): # n은 5자리수
     while True:
         guess_num = input("수를 입력하세요.\n입력: ")
         
         if len(guess_num) == n:
             return guess_num
-        else:
-            print(f'* {n}자리수를 입력하셔야 합니다!')
-            continue
 
